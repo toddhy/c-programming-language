cfe0cac413b1fa2a22dfb765bfae61abd434e40f
Todd <toddhy@hawaii.edu>
2014-07-07 08:30:20 -1000

    fds

---
+++ 1/1-18.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#define MAXLINE 1000
+
+void get(char line[], int max);
+void remtrail(char line[]);
+
+/* Remove trailing blanks and tabs, and delete totally blank lines */
+main()
+{
+	char line[MAXLINE];
+
+	get(line, MAXLINE);	
+	printf("%s", line);
+}
+
+/* get: read line into s */
+void get(char s[], int lim)
+{
+	int c, i;
+
+	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+		s[i] = c;
+	if (c == '\n') {
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+}
+
+/* remtrail: remove trailing spaces */
+void remtrail(char s[])
+{
+	int c, i, a;
+
+	a = 0;
+	while ((c = getchar)() != '\0')
+		++a;
+	for (i = a; i > 0; --i)
+		if (s[i] == ' ' || s[i] == '\t')
+		
